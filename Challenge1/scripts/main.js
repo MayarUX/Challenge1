95a7f9ebb435e753565534ce22bdf7be69bb4bed
MayarUX <76849908+MayarUX@users.noreply.github.com>
2021-05-19 10:29:11 +0200

    Add files via upload

--- Challenge1/scripts/main.js
+++ Challenge1/scripts/main.js
@@ -1,15 +1,29 @@
 //Selecting an item from the food or water inventory
 var item;
+var food = 25;
+var water = 50;
 function itemSelection (item){
     var inventoryElements = document.getElementById('foodInventory').children;
     for(var i = 0; i < inventoryElements.length; i++){
         inventoryElements[i].className = 'fullOpacity';
     }
     item.className = 'changeOpacity';
-    var food = 25;
+    
     food = food-1;
     document.getElementById('foodIndicator').innerHTML = '<p>' + 'Food: ' + food + '</p>';
 }
+
+//function itemSelection (item){
+//    var inventoryElements = document.getElementById('waterInventory').children;
+//    for(var i = 0; i < inventoryElements.length; i++){
+//        inventoryElements[i].className = 'fullOpacity';
+//    }
+//    item.className = 'changeOpacity';
+//    
+//    food = food-1;
+//    document.getElementById('waterIndicator').innerHTML = '<p>' + 'Water: ' + water + '</p>';
+//}
+
 
 //Converting Miles
 function marsMilesConverter (miles) {
