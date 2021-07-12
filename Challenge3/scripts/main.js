d9b7bf7df3e8fc7f418c7217812aab685ad8966b
MayarUX <76849908+MayarUX@users.noreply.github.com>
2021-07-12 09:33:47 +0200

    Add files via upload

--- Challenge3/scripts/main.js
+++ Challenge3/scripts/main.js
@@ -18,15 +18,15 @@ function getAPIdata() {
 
         var degC = Math.floor(response.main.temp - 273.25);
         document.getElementById('weatherTemprature').innerHTML = degC + ' &#176;C <br>';
-       
+
         document.getElementById('weatherDescription').innerHTML = response.weather[0].description;
 
-         document.getElementById('weatherWind').innerHTML = response.wind.speed + " km/h";
-        
+        document.getElementById('weatherWind').innerHTML = response.wind.speed + " km/h";
+
         document.getElementById('weatherHumidity').innerHTML = response.main.humidity + "%"; 
-        
+
         document.getElementById('weatherClouds').innerHTML = response.clouds.all + "%";
-        
+
         document.getElementById('weatherVisibility').innerHTML = response.visibility + " m";
     })
         .catch(function (error) {
@@ -50,13 +50,21 @@ function getAPINasa() {
     })
         .then(function(response2) {
 
-         document.getElementById('earthImage').innerHTML = response2;
+        //         document.getElementById('earthImage').innerHTML = response2;
+        console.log(response2);
+        document.getElementById('imageTitle').innerHTML = response2.title;
+        document.getElementById('imageDate').innerHTML = response2.date;
+        document.getElementById('imageDes').innerHTML = response2.explanation;
+        var imageSource = response2.url;
+        document.getElementById('earthImage').src = imageSource;
+
+
     })
         .catch(function (error) {
         console.log('Request failed', error);
     });
 }
-
-document.getElementById('earthImage').onclick = function(){
+document.getElementById('changeImage').onclick = function(){
     getAPINasa();
+
 };
